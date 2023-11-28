b208afc0ac525ee0a74bb8aa2440bc30fb698b2d
Donkaderon <a.chagdali00@gmail.com>
2023-11-28 12:36:17 +0100

    lastp

--- 6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-full_number = ""
-big_full_number = ""
-
-for i in range(0, 80):
-    number = "{:02d}".format(i)
-    full_number += number + ","
-    #big_full_number = full_number + "89"
-    print(full_number)
--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,4 +1,7 @@
-for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i != 99:
-        print(", ", end="")
+for i in range(10):
+    for j in range(10):
+        print("{:d}{:d}".format(i, j), end="")
+        if i != 9 or j != 9:
+            print(", ", end="")
+
+print()

65399daa40aa7e25e2efa25c3d4659d33b2c8811
Donkaderon <a.chagdali00@gmail.com>
2023-12-07 19:55:54 +0100

    p02

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-add_module = __import__('add_0', fromlist=['add'])
-result = add_module.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    add_module = __import__('add_0', fromlist=['add'])
+    result = add_module.add(a, b)
+    output = "{} + {} = {}".format(a, b, result)
+    print(output)

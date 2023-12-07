be183fc5e49a5837b04863065c2908a5b0e69e22
Donkaderon <a.chagdali00@gmail.com>
2023-12-07 20:01:02 +0100

    p03

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,8 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    add_module = __import__('add_0', fromlist=['add'])
+    add_module = __import__('add_0')
+    add_function = getattr(add_module, 'add')
     result = add_module.add(a, b)
     output = "{} + {} = {}".format(a, b, result)
     print(output)

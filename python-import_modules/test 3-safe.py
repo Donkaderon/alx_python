638129bb4043995e2413e15fd4127c28033ec980
Donkaderon <a.chagdali00@gmail.com>
2023-12-07 21:45:00 +0100

    p34

--- python-import_modules/test 3-safe.py
+++ python-import_modules/test 3-safe.py
@@ -1,6 +1,6 @@
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-a = 10
-b = 2
+a = 0
+b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))

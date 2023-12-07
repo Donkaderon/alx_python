ea6b321deba2c7ae59fbd73b2f8b0469c3296c72
Donkaderon <a.chagdali00@gmail.com>
2023-12-07 21:41:23 +0100

    p32

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,11 +4,15 @@ def safe_print_division(a, b):
         print(x)
     except ZeroDivisionError:
         print("Oops! b must not be equal to 0")
-
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Oops! b must not be equal to 0")
     finally:
         if 'result' in locals():
-            print("Inside result: {:.1f}".format(result))
-            print("{:d} / {:d} = {:.1f}".format(a, b, result))
+            print("Inside result: {}".format(result))
+            return result
         else:
             print("Inside result: None")
-            print("{:d} / {:d} = None".format(a, b))
+            return None

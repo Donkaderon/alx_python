79adb96d5c66b0b37e4a61aa0c66d07c474e13ee
Donkaderon <a.chagdali00@gmail.com>
2023-12-08 01:14:55 +0100

    none

--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -22,6 +22,6 @@ print_matrix_integer()
 #PROJECT 3
 multiple_returns = __import__('2-multiple_returns').multiple_returns
 
-sentence = "At Holberton school, I learnt C!"
+sentence = ""
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))

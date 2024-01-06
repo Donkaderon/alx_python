c874b5f2bb80894dd13f7bc6b569df74edc349c0
Donkaderon <a.chagdali00@gmail.com>
2024-01-06 16:44:19 +0100

    doc3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,15 +9,23 @@ Classes:
 """
 
 class Square:
+    """
+    Square class represents a square shape.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a Square object with a given size.
+
+    """
+
     def __init__(self, size=0):
         """
-        Square class represents a square shape.
+        Initializes a Square object with a given size.
 
         Args:
             size (int): The size of the square. Default is 0.
-
-        Attributes:
-            __size (int): Private instance attribute representing the size of the square.
 
         """
         self.__size = size

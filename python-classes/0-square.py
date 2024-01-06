62667a1b69aea638d2ff95cfeb42f317f105a354
Donkaderon <a.chagdali00@gmail.com>
2024-01-06 16:41:49 +0100

    documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 class Square:
-    def __init__(self, size=0) :
+    def __init__(self, size=0):
+        """
+        Square class represents a square shape.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Attributes:
+            __size (int): Private instance attribute representing the size of the square.
+
+        """
         self.__size = size

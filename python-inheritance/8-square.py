5b8ca9d2264890adaaafb8540d8501831637d73c
Donkaderon <a.chagdali00@gmail.com>
2024-01-07 20:12:35 +0100

    p8doc

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -83,6 +83,16 @@ class Rectangle(BaseGeometry):
         return f"[Rectangle] {self.__width}/{self.__height}"
     
 class Square(Rectangle):
+    """
+        Initializes a new instance of the Square class.
+
+        Args:
+        - size (int): The size of the square.
+
+        Raises:
+        - TypeError: If the size is not an integer.
+        - ValueError: If the size is less than or equal to 0.
+        """
     def __init__(self, size=0):
         self.__size = size
         super().__init__(size, size)  # Pass 'size' as both 'width' and 'height'

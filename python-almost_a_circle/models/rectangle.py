06bf1f255af46f02ff4f1408b3b00a013a7d0847
Donkaderon <a.chagdali00@gmail.com>
2024-01-11 13:43:54 +0100

    p7-t1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -185,3 +185,9 @@ class Rectangle(Base):
         """
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
+    def update(self, *args):
+        self.id = args[0]
+        self.__width = args[1]
+        self.__height = args[2]
+        self.__x = args[3]
+        self.__y = args[4]

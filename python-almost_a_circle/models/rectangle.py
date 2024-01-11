f2454e532e70b091e9db2b27d84a46d30ed693d1
Donkaderon <a.chagdali00@gmail.com>
2024-01-11 13:36:03 +0100

    p6-t6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -165,7 +165,7 @@ class Rectangle(Base):
         $##
         """
         for _ in range(self.__y):
-            print(' ')
+            print('')
 
         for _ in range(self.__height):
             for _ in range(self.__x):
@@ -184,11 +184,4 @@ class Rectangle(Base):
          - A string representation of the rectangle.
         """
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
-
-          
-
- 
-
-r = Rectangle(5, 4, 4, 3)
-r.display()
 

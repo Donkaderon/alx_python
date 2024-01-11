21b69baccd2cec8937ddc588cdba4e8a3851bb4d
Donkaderon <a.chagdali00@gmail.com>
2024-01-11 14:18:33 +0100

    p7-t2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -213,7 +213,15 @@ class Rectangle(Base):
         # Now the rectangle's width is 8 and height is 6
     """
         self.id = args[0]
-        self.__width = args[1]
-        self.__height = args[2]
-        self.__x = args[3]
-        self.__y = args[4]
+
+        if len(args) >= 2:
+            self.__width = args[1]
+
+        if len(args) >= 3:
+            self.__height = args[2]
+        
+        if len(args) >= 4:
+            self.__x = args[3]
+        
+        if len(args) >= 5:
+            self.__y = args[4]
